8d91780091fbb2673d1162a3c7d78f812f2f6ad5
AM <anvar.minulin@gmail.com>
2019-08-28 23:52:57 +0300

    Dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+#Dictionaries
+#Dictionary is mutable data structure
+
+my_dict = {}
+another_dict = dict()
+my_other_dict = {"one":1, "two":2, "three":3}
+my_other_dict #returns {'three': 3, 'two': 2, 'one': 1}
