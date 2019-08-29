da3f5e681f28ab3d6bea362537ccce911dd14740
AM <anvar.minulin@gmail.com>
2019-08-29 23:58:45 +0300

    Dictionaries, special characters

--- dictionaries.py
+++ dictionaries.py
@@ -5,3 +5,14 @@ my_dict = {}
 another_dict = dict()
 my_other_dict = {"one":1, "two":2, "three":3}
 my_other_dict #returns {'three': 3, 'two': 2, 'one': 1}
+my_other_dict["one"] #will teturn 1
+my_dict = {"name":"Fr", "address":"123 Elm street"}
+my_dict["name"] #return 'Fr'
+
+"name" in my_dict #will return True
+"state" in my_dict #will return False
+
+my_dict.keys() #will return dict_keys(['name', 'address'])
+
+"name" in my_dict # this is good
+"name" in my_dict.keys() # this works too, but is slower
---
+++ special_characters.py
@@ -0,0 +1,13 @@
+print("I have a \n new line in the middle")
+I have a
+new line in the middle
+print("This sentence is \ttabbed!")
+This sentence is    tabbed!
+
+print("This is a backslash \")
+Traceback (most recent call last):
+File "<string>", line 1, in <fragment>
+EOL while scanning string literal: <string>, line 1, pos 30
+
+print("This is a backslash \\")
+This is a backslash \
