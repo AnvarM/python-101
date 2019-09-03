5035c489e28d7c1e2c1ad2d002ece1ab12cd6fb3
AM <anvar.minulin@gmail.com>
2019-09-03 23:05:25 +0300

    os module - Part I

---
+++ os.py
@@ -0,0 +1,6 @@
+#os module
+os.name # return name of operating system, in case of windows will return 'nt'
+os.environ # return dictionary with operating system environment variables
+os.getenv("TMP") # will return value for environment variable TMP
+os.getcwd() # will return current path
+os.chdir(r"C:\\temp") # will change current directory to C:\temp
